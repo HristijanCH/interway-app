5d636c0a5b300f952c9a33b3fe9fce3edf1534ad
SmartW0rker <hristijanchochevski@gmail.com>
2025-05-18 11:14:30 +0200

    initial commit

--- src/main/java/com/kiko/interwayapp/exceptions/ProductNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.kiko.interwayapp.exceptions;
-
-import java.util.UUID;
-
-public class ProductNotFoundException extends RuntimeException{
-    public ProductNotFoundException(UUID id) {
-        super(String.format("Product with id: %s does not exist",id));
-    }
-}
